fa362efd88565ba0fdae30e458e8e14d82303606
r24xtech <57235265+r24xtech@users.noreply.github.com>
2019-11-14 16:52:50 -0300

    Incomplete doubly linked list with sorting/swaping

---
+++ za.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+  "fmt"
+  "github.com/r24xtech/myutils"
+)
+
+type Node struct {
+  value int
+  next *Node
+  previous *Node
+}
+
+type List struct {
+  size int
+  head *Node
+  tail *Node
+  keep *Node
+}
+
+// next goes to the back
+// previous goes to the front
+func (l *List) InsertBack() {
+  n := new(Node)
+  x := myutils.Get_int("Enter value: ")
+  n.value = x
+  if l.size == 0 {
+    l.head = n
+    l.tail = l.head
+  } else {
+    l.tail.next = n
+    n.previous = l.tail
+    l.tail = n
+  }
+  l.size++
+}
+
+func (l *List) InsertFront () {
+  n := new(Node)
+  x := myutils.Get_int("Enter value: ")
+  n.value = x
+  if l.size == 0 {
+    l.head = n
+    l.tail = l.head
+  } else {
+    l.head.previous = n
+    n.next = l.head
+    l.head = n
+  }
+  l.size++
+}
+
+func (l *List) Print() {
+  if l.size == 0 {
+    fmt.Println("[...] the list is empty.")
+  } else {
+    l.keep = l.head
+    fmt.Println()
+    fmt.Print("front| ")
+    for {
+      fmt.Print(l.head.value, " ")
+      if l.head.next == nil {
+        break
+      }
+      l.head = l.head.next
+    }
+    fmt.Print(" |back")
+    fmt.Println("\n")
+    l.head = l.keep
+  }
+}
+
+func (l *List) RemoveFront() {
+  if l.size == 0 {
+    fmt.Println("Can't remove...the list is empty!")
+  } else if l.size == 1 {
+    l.head = l.head.next
+    l.size--
+  } else {
+    l.head = l.head.next
+    l.head.previous = nil
+    l.size--
+  }
+}
+
+func (l *List) RemoveBack() {
+  if l.size == 0 {
+    fmt.Println("Can't remove...the list is empty!")
+  } else if l.size == 1 {
+    l.tail = l.tail.previous
+    l.size--
+  } else {
+    l.tail = l.tail.previous
+    l.tail.next = nil
+    l.size--
+  }
+}
+
+/* it breaks the program
+func (l *List) Swap() {
+  tmp := new(Node)
+  tmp = l.head.next
+  l.head.next = l.head.next.next
+  tmp.next = l.head
+  l.head = tmp
+}
+*/
+
+func (l *List) Swap() {
+  if l.size == 0 || l.size == 1 {
+    fmt.Println("Can't swap!")
+  } else if l.size == 2 {
+    l.tail.next = l.head
+    l.head.previous = l.tail
+    l.head.next = nil
+    l.tail.previous = nil
+    l.tail = l.tail.next
+    l.head = l.head.previous
+    //l.keep = nil // added
+  } else {
+    l.keep = l.head.next
+    l.head.next = l.head.next.next
+    l.head.next.previous = l.head
+    l.head = l.keep
+    l.head.next = l.keep.previous
+    l.head.next.previous = l.head
+    l.keep = nil // added
+  }
+}
+
+func (l *List) Sort() {
+  l.Swap()
+  
+}
+
+/*
+func (l *List) Sort() {
+  t := 1
+  for {
+    count := 0
+    for i:= 0; i < l.size - t; i++ {
+      if l.head.value < l.head.next.value {
+        l.Swap()
+        count++
+      }
+      //l.head = l.head.next
+    }
+    if count == 0 {
+      break
+    }
+  t++
+  }
+}
+*/
+
+func main() {
+  l := new(List)
+
+  fmt.Println("|| Welcome ||")
+  fmt.Println("[1] InsertBack; [2] InsertFront; [3] Print;")
+  fmt.Println("[4] RemoveBack; [5] RemoveFront; [6] End;\n\n")
+
+  myloop:for {
+    option := myutils.Get_int("Choose option: ")
+    switch option {
+    case 0: l.Sort()
+    case 1: l.InsertBack()
+    case 2: l.InsertFront()
+    case 3: l.Print()
+    case 4: l.RemoveBack()
+    case 5: l.RemoveFront()
+    case 6: break myloop
+    default: fmt.Println("Invalid option.")
+    }
+  }
+}
